5aebd1225793f1e90856f1183418de629d4b486a
B.Pradhan <bpradhan17120@gmail.com>
2026-08-21 11:44:48 +0530

    order.routes.js

--- frontend/src/pages/food-partner/Profile.jsx
+++ frontend/src/pages/food-partner/Profile.jsx
@@ -60,7 +60,7 @@ const Profile = () => {
           <div style={{ display: 'flex', alignItems: 'center', gap: 20, flexWrap: 'wrap' }}>
             
             <img 
-              src="https://images.unsplash.com/photo-1754653099086-3bddb9346d37?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0Nnx8fGVufDB8fHx8fA%3D%3D" 
+              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
               alt="Avatar" 
               style={{ width: 80, height: 80, borderRadius: '50%', objectFit: 'cover', border: '2px solid #eab308' }}
             />
